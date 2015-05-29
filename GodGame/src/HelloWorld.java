60e8aed4ff789691f4eaa281aa8998a7db0173d1
mshaw11 <mshaw11@qub.ac.uk>
2015-05-29 22:14:45 +0100

    Commit attempt 2.

--- GodGame/src/HelloWorld.java
+++ GodGame/src/HelloWorld.java
@@ -10,6 +10,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Matt uploaded this to git hub!");
+		System.out.println("Attempt 2 from matt");
 
 	}
 
