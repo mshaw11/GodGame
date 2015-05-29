c568f42aebba5723fdcfa2ccc24ce721c288e48c
mshaw11 <mshaw11@qub.ac.uk>
2015-05-29 22:02:48 +0100

    First Commit.

---
+++ GodGame/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Matt uploaded this to git hub!");
+
+	}
+
+}
