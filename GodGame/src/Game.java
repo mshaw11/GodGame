842966a7097c21c89b38fd25b0ccd782d34560da
Ryan3555 <redwards03@qub.ac.uk>
2015-06-02 13:19:57 +0100

    Added Game loop to GameTimer class

--- GodGame/src/Game.java
+++ GodGame/src/Game.java
@@ -2,31 +2,22 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 
-public class Game implements Runnable {
+public class Game {
 	
 	private boolean running = false;
 	private Thread thread;
+	static GameTimer gameLoop;
 	
 	public static void main(String [] args){
 		Game game = new Game();
-		game.start();
+		gameLoop = new GameTimer();
 	}
 	
 	public Game() {
-		run();
 		//Set up relevant objects - players etc.
 		//Start game timer
 		//
-		//setupGameWindow();
-	}
-	
-	
-	public void setupGameWindow(){
-		SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI(); 
-            }
-        });
+		createAndShowGUI();
 	}
 	
 	 private static void createAndShowGUI() {
@@ -41,77 +32,7 @@ public class Game implements Runnable {
 	        f.pack();
 	    }
 	 
-	 public void init(){
-		 
-	 }
-	 
-	 public void tick(){
-		 
-	 }
-	 
-	 public void render(){
-		 
-	 }
 
-	@Override
-	public void run() {
-		createAndShowGUI();
-		init();
-		long lastTime = System.nanoTime();
-		final double numTicks = 60.0;
-		double n = 1000000000/numTicks;
-		double delta = 0;
-		int frames = 0;
-		int ticks = 0;
-		long timer = System.currentTimeMillis();
-		
-		while(running){
-			long currentTime = System.nanoTime();
-			delta += (currentTime - lastTime) / n;
-			lastTime = currentTime;
-			
-			if (delta >= 1){
-				ticks++;
-				delta--;
-			}
-			
-			render();
-			frames++;
-			
-			if (System.currentTimeMillis()-timer>1000){
-				timer+=1000;
-				System.out.println(ticks+" Ticks, FPS: "+ frames);
-				ticks=0;
-				frames=0;
-			}
-			
-		}
-		
-	} 
-	
-	private synchronized void start(){
-		if (running){
-			return;
-		} else {
-			running = true;
-			thread = new Thread(this);
-			thread.start();
-		}
-	}
-	
-	private synchronized void stop(){
-		if (!running){
-			return;
-		} else {
-			running = false;
-			try {
-				thread.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			System.exit(1);
-		}
-	}
 
 
 	
--- GodGame/src/GameTimer.java
+++ GodGame/src/GameTimer.java
@@ -1,8 +1,93 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-public class GameTimer {
 
+public class GameTimer implements Runnable {
+
+	private boolean running = false;
+	private Thread thread;
+	
+	public static void main(String [] args){
+		Game game = new Game();
+	}
+	
 	public GameTimer() {
-		// TODO Auto-generated constructor stub
+		start();
+
+	}
+	 
+	 public void init(){
+		 
+	 }
+	 
+	 public void update(){
+		 
+	 }
+	 
+	 public void render(){
+		 
+	 }
+
+	@Override
+	public void run() {
+		init();
+		long lastTime = System.nanoTime();
+		final double numTicks = 60.0;
+		double n = 1000000000/numTicks;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
+		long timer = System.currentTimeMillis();
+		
+		while(running){
+			long currentTime = System.nanoTime();
+			delta += (currentTime - lastTime) / n;
+			lastTime = currentTime;
+			
+			if (delta >= 1){
+				update();
+				updates++;
+				delta--;
+			}
+			
+			render();
+			frames++;
+			
+			if (System.currentTimeMillis()-timer>1000){
+				timer+=1000;
+				System.out.println(updates+" Ticks, FPS: "+ frames);
+				updates=0;
+				frames=0;
+			}
+			
+		}
+		
+		stop();
+		
+	} 
+	
+	private synchronized void start(){
+		if (running){
+			return;
+		} else {
+			running = true;
+			thread = new Thread(this);
+			thread.start();
+		}
+	}
+	
+	private synchronized void stop(){
+		if (!running){
+			return;
+		} else {
+			running = false;
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.exit(1);
+		}
 	}
 
 }
