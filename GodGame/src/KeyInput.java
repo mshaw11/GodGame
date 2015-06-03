b40ba8f379695cd9686b52665db1bd1f8190d2d5
Ryan3555 <redwards03@qub.ac.uk>
2015-06-03 22:21:33 +0100

    Commented KeyInput class

--- GodGame/src/KeyInput.java
+++ GodGame/src/KeyInput.java
@@ -10,16 +10,20 @@ public class KeyInput extends KeyAdapter {
 		this.handler = handler;
 	}
 	
+	//Called when key is pressed	
 	public void keyPressed(KeyEvent e){
 		int key = e.getKeyCode();
-		System.out.println(key);
+		//System.out.println(key);
 		
+		//For loop to go through game objects in handler
 		for (int i = 0;i<handler.object.size();i++){
 			GameObject tempObject = handler.object.get(i);
 			
+			//If ID of tempObject is equal to Player
 			if (tempObject.getID()==ID.Player){
 				//KEY EVENTS FOR PLAYER
 				
+				//If key is == W decrease velY by 5 and so on
 				if (key==KeyEvent.VK_W) tempObject.setVelY(-5);
 				if (key==KeyEvent.VK_A) tempObject.setVelX(-5);
 				if (key==KeyEvent.VK_S) tempObject.setVelY(5);
@@ -40,16 +44,19 @@ public class KeyInput extends KeyAdapter {
 		
 	}
 	
+	//Called when key is released
 	public void keyReleased(KeyEvent e){
 		int key = e.getKeyCode();
 		System.out.println(key);
 		
+		//For loop to go through game objects in handler
 		for (int i = 0;i<handler.object.size();i++){
 			GameObject tempObject = handler.object.get(i);
 			
 			if (tempObject.getID()==ID.Player){
 				//KEY EVENTS FOR PLAYER
 				
+				//If key is == W set velY to 0 and so on
 				if (key==KeyEvent.VK_W) tempObject.setVelY(0);
 				if (key==KeyEvent.VK_A) tempObject.setVelX(0);
 				if (key==KeyEvent.VK_S) tempObject.setVelY(0);
